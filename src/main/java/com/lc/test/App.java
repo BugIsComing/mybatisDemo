c8aac4864bd5c04bf51487d75e27c74d2fb5d253
刘诚 <LC513901@163.com>
2018-06-15 20:16:08 +0800

    2018年6月15日20:16:01
    继续学习

--- src/main/java/com/lc/test/App.java
+++ src/main/java/com/lc/test/App.java
@@ -11,12 +11,10 @@ import java.util.*;
 
 /**
  * @author ifly_lc
- *  配置方式参考官网http://www.mybatis.org/mybatis-3/zh/getting-started.html
+ * 配置方式参考官网http://www.mybatis.org/mybatis-3/zh/getting-started.html
  */
-public class App
-{
-    public static void main( String[] args )
-    {
+public class App {
+    public static void main(String[] args) {
         /**
          * 将运行时生成的代理类（.class文件）输出到本地路径下；
          */
@@ -29,45 +27,49 @@ public class App
 
 
         //执行查询返回一个唯一user对象的sql
-        Map<String,Object> params = new HashMap<String,Object>();
+        Map<String, Object> params = new HashMap<String, Object>();
 
-
-        params.put("id",45);
-        params.put("age",2);
-        params.put("name", String.valueOf(new Random().nextInt(10)));
-        session.update("updateUser",params);
-        //params.put("name","lc");
-        params.clear();
-        params.put("age",2);
-        List<User> userList = null;
-        //userList = userMapper.getUserByParam(params);
+        /*************通过API方式访问mybatis，进行CRUD操作************/
+        /**
+         * C操作
+         */
+        int temp = new Random().nextInt(10);
+        params.put("name", "LC");
+        params.put("age", temp);
+        session.insert("saveUser", params);
 
         /**
-         * 通过传统的API方式访问mybatis
+         * U操作
          */
-        userList =  session.selectList("getUserByParam",params);
+        params.clear();
+        params.put("id", 45);
+        params.put("age", 2);
+        params.put("name", String.valueOf(new Random().nextInt(10)));
+        session.update("updateUser", params);
+        /**
+         * R操作
+         */
+        params.clear();
+        params.put("age", 2);
+        List<User> userList;
+        userList = session.selectList("getUserByParam", params);
 
-        for (User ur : userList){
+        for (User ur : userList) {
             System.out.println(ur.toString());
         }
 
+        /**
+         * D操作
+         */
+        params.clear();
+        params.put("age",temp);
+        session.delete("deleteUser",params);
         /**
          * session必须提交，否则无法写入到数据库
          * 如果在Configuration.xml中设置了defaultAutoCommit，则不需要这一行
          */
         //session.commit();
 
-        /**
-         * 通过Mapper接口访问mybatis
-         * 这一句执行完之后会在com.sun.proxy路径下生成一个代理类，前提是将sun.misc.ProxyGenerator.saveGeneratedFiles设置为true
-         */
-        //UserMapper userMapper = session.getMapper(UserMapper.class);
-//        User user = new User();
-//        for(int i=1;i<=10;i++){
-//            user.setName("LC");
-//            user.setAge(i);
-//            userMapper.saveUser(user);
-//        }
         session.close();
     }
 }

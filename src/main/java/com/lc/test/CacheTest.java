a163bad217c1a7398e26772f670a90e1f71e65d8
刘诚 <LC513901@163.com>
2018-06-25 17:38:36 +0800

    2018年6月25日17:38:31
    继续学习

--- src/main/java/com/lc/test/CacheTest.java
+++ src/main/java/com/lc/test/CacheTest.java
@@ -9,14 +9,13 @@ import java.io.InputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 
 /**
- * @author
- * 测试验证mybatis提供的缓存功能，以App这个类中的测试接口
+ * @author 测试验证mybatis提供的缓存功能，以App这个类中的测试接口
+ * 主要测试mybatis提供的一级缓存功能
  */
 public class CacheTest {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         String resource = "Configuration.xml";
         InputStream is = App.class.getClassLoader().getResourceAsStream(resource);
         SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build(is);
@@ -41,7 +40,11 @@ public class CacheTest {
         userList = session.selectList("getUserByParam", params);
 
         /**
-         * 试验二，同一个session中先查询一次，然后插入数据，最后在查询同一个SQL语句，判断缓存是否失效
+         * 每次试验完之后调用清理一级缓存
+         */
+        session.clearCache();
+        /**
+         * 试验二，同一个session中先查询一次，然后插入数据（此处插入一个会影响第一次查询结果的记录），最后在查询同一个SQL语句，判断缓存是否失效
          */
         System.out.println("------------------------------试验二--------------------------------------------");
         /**
@@ -54,21 +57,22 @@ public class CacheTest {
         /**
          * 插入语句
          */
-        int temp = new Random().nextInt(10);
+        int temp = 2;
         params.clear();
-        params.put("name", "LC");
+        params.put("name", "LC222");
         params.put("age", temp);
         session.insert("saveUser", params);
         /**
-         * 再次查询
+         * 再次查询,结果增加了一条记录，说明insert、update等操作会影响缓存
          */
         params.clear();
         params.put("age", 2);
         userList = session.selectList("getUserByParam", params);
+        session.clearCache();
         System.out.println("------------------------------试验三--------------------------------------------");
         /**
-         * 不同session中，一个session1先查询，另一个session2再插入（此处插入一个会影响第一次查询结果的记录），session1再次
-         * 查询同一个SQL
+         * 不同session中，一个session1先查询，另一个session2再插入（此处插入一个会影响第一次查询结果的记录），session1再次查询同一个SQL
+         * 试验结果表明，不同session的插入不会影响其他session的缓存结果，除非缓存失效再次查询数据库
          */
         /**
          *
@@ -109,6 +113,29 @@ public class CacheTest {
         for (User ur : userList) {
             System.out.println(ur.toString());
         }
+        session.clearCache();
+        session1.clearCache();
+
+        /**
+         * 试验四，两次查询，两次查询的SQL处理where之后的条件不一样，其他都一样，where条件的顺序不一样
+         * 试验四是试验一的变型
+         * 试验结果表明查询条件的顺序会影响缓存,查询条件的顺序不同，也会被当做不同的缓存存放（虽然结果是相同的），
+         * 引起缓存不同的原因是两个查询的statementId不一样导致的,实际开发中不应该出现这种查询情况，应该是要getUserByParam这种方式
+         */
+        System.out.println("------------------------------试验四--------------------------------------------");
+        params.clear();
+        params.put("age", 2);
+        params.put("name","LC");
+        userList = session.selectList("getUserByAgeAndName", params);
+
+        /**
+         * 再次查询，交换查询条件顺序
+         */
+        userList = session.selectList("getUserByNameAndAge", params);
+        session.clearCache();
+
+        session.close();
+        session1.close();
     }
 
 }
